2b61b416f182579bc8e58f2fa6dc57890f9f4388
asnazarov <asn.nazarov.asn@gmail.com>
2020-03-18 19:17:44 +0300

    create header

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,18 @@
-$(function() {
+// $(function() {
 
 
 
-    $('select').styler();
+//     $('select').styler();
 
-    $('.hero__button').on('click', function() {
-        $('.menu ul').slideToggle();
-    });
+//     $('.hero__button').on('click', function() {
+//         $('.menu ul').slideToggle();
+//     });
 
+// });
+
+jQuery(function($) {
+    $("#name").mask("А-я");
+    $("#phone").mask("0-9", { placeholder });
+    $("#tin").mask("99-9999999");
+    $("#ssn").mask("999-99-9999");
 });
