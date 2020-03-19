598b9f845696e4007ce1b77fe2ebd4cd5ad4dff3
asnazarov <asn.nazarov.asn@gmail.com>
2020-03-19 13:28:09 +0300

    create footer and email form

--- app/js/main.js
+++ app/js/main.js
@@ -16,3 +16,24 @@ jQuery(function($) {
     $("#tin").mask("99-9999999");
     $("#ssn").mask("999-99-9999");
 });
+
+$(document).ready(function() {
+
+    //E-mail Ajax Send
+    $("form").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            alert("Thank you!");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+
+});
